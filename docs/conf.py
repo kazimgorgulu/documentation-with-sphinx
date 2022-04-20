7b20a3d6f213ddfdeee1e8aa29b7a32b7f444f84
kazimgorgulu <kgorgulu18@ku.edu.tr>
2022-04-20 11:06:23 +0300

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,12 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath('../palgds/'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Crawler'
+project = 'palgds'
 copyright = '2022, Kazim Gorgulu'
 author = 'Kazim Gorgulu'
 
